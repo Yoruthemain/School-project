407c303e5ecf296bb41ad05ad038334821b58446
Yoruthemain <108297117+Yoruthemain@users.noreply.github.com>
2024-09-21 14:36:12 -0400

    upd

---
+++ public/assets/js/new.js
@@ -0,0 +1,12 @@
+console.log(student1);
+var student1 ="Nife"
+const teacher={trainer1:"Mr Tunde",Trainer2:"Mr. tony"}
+teacher['trainer1']="Mr.Emmanuel"
+console.log(student1,student2,teacher);
+
+if(student1,student2,teacher)
+if(student1="Nife"){
+    var student2="Muiz"
+    console.log(`${student2} is a great student.`);
+    
+}
